8cd7627cc5d791e975ec5c12020a6399d4d956e5
qiming2 <pguan2017@gmail.com>
2019-11-16 21:05:56 -0800

    FPS without thread

--- src/com/comf0rts/gameUtils/game/GameCore.java
+++ src/com/comf0rts/gameUtils/game/GameCore.java
@@ -13,7 +13,7 @@ public class GameCore implements Runnable{
 	private final int WIDTH = 432; // width of the window
 	private final int HEIGHT = 768; // height of the window
 	private final String TITLE = "Canvas Test"; // Window title
-	private final int FPS = 60; // How many frames per seconds
+	private final int FPS = 1; // How many frames per seconds
 	private Graphics g;
 	private Canvas canvas;
 	private BufferStrategy bs;
@@ -25,34 +25,46 @@ public class GameCore implements Runnable{
 			canvas.createBufferStrategy(1);
 			return;
 		}
+
+		//FPS independent implementation
+		//Nano second
+		double timePerTick = 1000000000 / FPS;
+		double delta = 0;
+		double timer = 0;
+		long now;
+		long lastTime = System.nanoTime();
 	    
 		// debug
 		int x = 0;
 	    int y = 0;
 		while(1==1) {
-			g = bs.getDrawGraphics();
-			//draw the bird
-			g.clearRect(0, 0, WIDTH, HEIGHT);
-			g.setColor(Color.red);
-			g.fillRect(x, y, 20, 20);
-			// end drawing
-			bs.show();
-			g.dispose();
-			if(x >= WIDTH) {
-				x = 0;
-				y+=20;
-				if( y >= HEIGHT) {
-					y = 0;
+			now = System.nanoTime();
+			delta = (now - lastTime) / timePerTick;
+			timer += delta;
+			lastTime = now;
+
+			if (timer >= 1) {
+				g = bs.getDrawGraphics();
+
+				//draw the bird
+				g.clearRect(0, 0, WIDTH, HEIGHT);
+				g.setColor(Color.red);
+				g.fillRect(x, y, 20, 20);
+				// end drawing
+				bs.show();
+				g.dispose();
+				if(x >= WIDTH) {
+					x = 0;
+					y += 20;
+					if( y >= HEIGHT) {
+						y = 0;
+					}
+				}else {
+					x += Math.ceil((double) 200 / FPS);
 				}
-			}else {
-				x+=10;
+				timer--;
 			}
-			try {
-				Thread.sleep(1000 / FPS);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
 		}
 		
 	}
